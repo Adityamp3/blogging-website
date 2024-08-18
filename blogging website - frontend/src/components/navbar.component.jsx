e8755f6c2feaacf7e8ba1fcc2c826d91efdaa1fe
Aditya Raj Lodhi <dealwithaditya@gmail.com>
2024-08-19 00:03:40 +0530

    designing navbar

--- blogging website - frontend/src/components/navbar.component.jsx
+++ blogging website - frontend/src/components/navbar.component.jsx
@@ -1,7 +1,15 @@
-const Navbar = () =>{
-    return (
-        <h1>Navbar</h1>
-    )
-}
+import { Link } from "react-router-dom";
+import logo from "../imgs/logo.png";
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <Link to='/' className="flex-none w-10">
+        <img src={logo} alt="load nhi hui" />
+      </Link>
+
+      <div className="absolute bg-white w-full left-0 top-full mt-0.5 border-b border-grey"></div>
+    </nav>
+  );
+};
 
 export default Navbar;
