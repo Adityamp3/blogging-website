bac97567e6cf3d5c81aa11cacc1f764c8f4642a2
Aditya Raj Lodhi <dealwithaditya@gmail.com>
2024-08-28 12:01:31 +0530

    React Routes

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,8 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar.component';
+
 const App = () => {
+
     return (
-        <Navbar/>
+        <Routes>
+            <Route path="/" element={<Navbar />}>
+                <Route path="/editor" element={<h1>So you want to write, right?</h1>} />
+                <Route path="/signin" element={<h1>Sign In page</h1>} />
+                <Route path="/signup" element={<h1>Sign Un page</h1>} />
+            </Route>
+        </Routes>
     )
+
 }
 
 export default App;
