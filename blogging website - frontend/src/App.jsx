8b7a1ec25231a0e9247e9f6f7cc8e25c9c164b14
Aditya Raj Lodhi <dealwithaditya@gmail.com>
2024-08-30 17:03:14 +0530

    Setting up Routes

--- blogging website - frontend/src/App.jsx
+++ blogging website - frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar.component';
+import UserAuthForm from './pages/userAuthForm.page';
 
 const App = () => {
 
     return (
         <Routes>
             <Route path="/" element={<Navbar />}>
-                <Route path="/editor" element={<h1>So you want to write, right?</h1>} />
-                <Route path="/signin" element={<h1>Sign In page</h1>} />
-                <Route path="/signup" element={<h1>Sign Un page</h1>} />
+                <Route path="/editor" element={<h1>Writing...</h1>} />
+                <Route path="/signin" element={<UserAuthForm type="sign-in"/>} />
+                <Route path="/signup" element={<UserAuthForm type="sign-up"/>} />
             </Route>
         </Routes>
     )
