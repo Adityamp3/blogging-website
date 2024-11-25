da3c21cce4a4817c7941c0bfb9cfec2e3f217146
Aditya Raj Lodhi <dealwithaditya@gmail.com>
2024-11-26 05:07:31 +0530

    Blog Editor Done

--- blogging website - frontend/src/components/blog-editor.component.jsx
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -3,15 +3,28 @@ import logo from "../imgs/logo.png";
 import AnimationWrapper from "../common/page-animation";
 import defaultBanner from "../imgs/blog banner.png";
 import { uploadImage } from "../common/aws";
-import { useRef } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { Toaster, toast } from "react-hot-toast";
+import { EditorContext } from "../pages/editor.pages";
+import EditorJS from "@editorjs/editorjs";
+import { tools } from "./tools.component";
 
 const BlogEditor = () => {
-  let blogBannerRef = useRef();
+
+  // let blogBannerRef = useRef();
+  let { blog, blog: { title, banner, content, tags, des}, setBlog } = useContext(EditorContext);
+
+  useEffect(() => {
+    let editor = new EditorJS({
+      holderId: "textEditor",
+      data: '',
+      tools: tools,
+      placeholder: "Let's start a new story...",
+    })
+  }, []);
 
   const handleBannerUpload = (e) => {
     let img = e.target.files[0];
-    console.log(img);
 
     if (img) {
       let loadingToast = toast.loading("Uploading Image...");
@@ -21,7 +34,9 @@ const BlogEditor = () => {
           if (url) {
             toast.dismiss(loadingToast);
             toast.success("Uploaded 👍");
-            blogBannerRef.current.src = url;
+            // blogBannerRef.current.src = url;
+
+            setBlog({ ...blog, banner: url });
           }
         })
         .catch((err) => {
@@ -31,13 +46,32 @@ const BlogEditor = () => {
     }
   };
 
+  const handleTitleKeyDown = (e) => {
+    if (e.keyCode === 13) {      // Enter key
+      e.preventDefault();
+    }
+  };
+
+  const handleTitleChange = (e) => {
+    let input = e.target;
+    input.style.height = "auto";
+    input.style.height = input.scrollHeight + "px";
+
+    setBlog({ ...blog, title: input.value });
+  };
+
+  const handleError = (e) => {
+    let img = e.target;
+    img.src = defaultBanner;
+  };
+
   return (
     <>
       <nav className="navbar">
         <Link to="/" className="flex-none w-10">
           <img src={logo} />
         </Link>
-        <p className="max-md:hidden text-black line-clamp-1 w-full">New Blog</p>
+        <p className="max-md:hidden text-black line-clamp-1 w-full">{ title.length ? title : "New Blog" }</p>
 
         <div className="flex gap-4 ml-auto">
           <button className="btn-dark py-2">Publish</button>
@@ -51,7 +85,12 @@ const BlogEditor = () => {
           <div className="mx-auto max-w-[900px] w-full">
             <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
               <label htmlFor="uploadBanner">
-                <img ref={blogBannerRef} src={defaultBanner} className="z-20" />
+                <img 
+                // ref={blogBannerRef}
+                src={banner} 
+                className="z-20"
+                onError={handleError}
+                 />
                 <input
                   id="uploadBanner"
                   type="file"
@@ -61,6 +100,18 @@ const BlogEditor = () => {
                 />
               </label>
             </div>
+
+            <textarea
+              placeholder="Blog Title"
+              className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
+              onKeyDown={handleTitleKeyDown}
+              onChange={handleTitleChange}
+            ></textarea>
+
+            <hr className="w-full opacity-10 my-5"/>
+
+            <div id="textEditor" className="font-gelasio"></div>
+
           </div>
         </section>
       </AnimationWrapper>
--- blogging website - frontend/src/pages/editor.pages.jsx
+++ blogging website - frontend/src/pages/editor.pages.jsx
@@ -1,22 +1,40 @@
 import { useContext, useState } from "react";
 import { UserContext } from "../App";
 import { Navigate } from "react-router-dom";
-import PublishForm from "../components/blog-editor.component";
+import PublishForm from "../components/publish-form.component";
 import BlogEditor from "../components/blog-editor.component";
+import { createContext } from "react";
+
+const blogStructure = {
+  title: "",
+  banner: "",
+  content: "",
+  tags: [],
+  des: "",
+  author: { personal_info: {} },
+};
+
+export const EditorContext = createContext({});
 
 const Editor = () => {
-  const [editorState, setEditorState] = useState("publish");
+  const [blog, setBlog] = useState(blogStructure);
+  const [editorState, setEditorState] = useState("editor");
 
   let {
     userAuth: { access_token },
   } = useContext(UserContext);
 
-  return access_token === null ? (
-    <Navigate to="/signin" />
-  ) : editorState == "editor" ? (
-    <BlogEditor />
-  ) : (
-    <PublishForm />
+  return (
+    <EditorContext.Provider value={{blog, setBlog, editorState, setEditorState}}>
+      {
+      access_token === null ?
+      <Navigate to="/signin" />
+      : editorState == "editor" ?
+      <BlogEditor />
+      :
+      <PublishForm />
+      }
+    </EditorContext.Provider>
   );
 };
 
