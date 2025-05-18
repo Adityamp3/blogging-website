dd404583d2581b5e80e62502120e7dc76d10d538
Aditya Raj Lodhi <adityaraj@mail.astroarunpandit.org>
2025-05-18 18:26:11 +0530

    [feat] Save Draft Done

--- blogging website - frontend/src/components/blog-editor.component.jsx
+++ blogging website - frontend/src/components/blog-editor.component.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../imgs/logo.png";
 import AnimationWrapper from "../common/page-animation";
 import defaultBanner from "../imgs/blog banner.png";
@@ -8,11 +8,15 @@ import { Toaster, toast } from "react-hot-toast";
 import { EditorContext } from "../pages/editor.pages";
 import EditorJS from "@editorjs/editorjs";
 import { tools } from "./tools.component";
+import axios from "axios";
+import { UserContext } from "../App";
 
 const BlogEditor = () => {
 
   // let blogBannerRef = useRef();
   let { blog, blog: { title, banner, content, tags, des}, setBlog, textEditor, setTextEditor, setEditorState } = useContext(EditorContext);
+  let { userAuth: { access_token } } = useContext(UserContext)
+  let navigate = useNavigate()
 
   useEffect(() => {
     setTextEditor(new EditorJS({
@@ -93,6 +97,55 @@ const BlogEditor = () => {
 
   };
 
+  const handleSaveDraft = (e) => {
+
+    if(e.target.className.includes("disable")){
+      return;
+    }
+    
+    if(!title.length) {
+      return toast.error("Write blog title before saving it as a draft")
+    }
+
+    let loadingToast = toast.loading("Saving Draft...");
+
+    e.target.classList.add('disable');
+
+    if(textEditor.isReady){
+      textEditor.save().then( content => {
+
+        let blogObj = {
+          title, banner, des, content, tags, draft: true
+        }
+        
+        axios.post(import.meta.env.VITE_SERVER_DOMAIN + "/create-blog", blogObj, {
+          headers: {
+            'Authorization': `Bearer ${access_token}`
+          }
+        })
+        .then(()=>{
+      
+          e.target.classList.remove('disable');
+    
+          toast.dismiss(loadingToast);
+          toast.success("Saved 👍");
+    
+          setTimeout(() => {
+            navigate("/")
+          }, 500);
+        })
+        .catch(( { response } ) => {
+          e.target.classList.remove('disable');
+            toast.dismiss(loadingToast);
+            
+            return toast.error(response.data.error);
+        })
+
+      })
+    }
+
+  }
+
   return (
     <>
       <nav className="navbar">
@@ -103,7 +156,7 @@ const BlogEditor = () => {
 
         <div className="flex gap-4 ml-auto">
           <button className="btn-dark py-2" onClick={handlePublishEvent}>Publish</button>
-          <button className="btn-light py-2">Save Draft</button>
+          <button className="btn-light py-2" onClick={handleSaveDraft}>Save Draft</button>
         </div>
       </nav>
       <Toaster />
