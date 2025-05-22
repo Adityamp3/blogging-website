4aebf1efc5ba046bdefeeab670c5a98d4e288a99
Aditya Raj Lodhi <adityaraj@mail.astroarunpandit.org>
2025-05-23 00:49:30 +0530

    [feat] Implement Pagination and load more functionality for blogs

--- blogging website - frontend/src/pages/home.page.jsx
+++ blogging website - frontend/src/pages/home.page.jsx
@@ -7,6 +7,8 @@ import BlogPostCard from "../components/blog-post.component";
 import MinimalBlogPost from "../components/nobanner-blog-post.component";
 import { activeTabRef } from "../components/inpage-navigation.component";
 import NoDataMessage from "../components/nodata.component";
+import { filterPaginationData } from "../common/filter-pagination-data";
+import LoadMoreDataBtn from "../components/load-more.component";
 
 const HomePage = () => {
   let [blogs, setBlog] = useState(null);
@@ -23,22 +25,40 @@ const HomePage = () => {
     "lifestyle",
   ];
 
-  const fetchLatestBlogs = () => {
+  const fetchLatestBlogs = ({ page = 1 }) => {
     axios
-      .get(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs")
-      .then(({ data }) => {
-        setBlog(data.blogs);
+      .post(import.meta.env.VITE_SERVER_DOMAIN + "/latest-blogs", { page })
+      .then( async ({ data }) => {
+
+        let formatedData = await filterPaginationData({
+          state: blogs,
+          data: data.blogs,
+          page,
+          countRoute: "/all-latest-blogs-count"
+        })
+
+        setBlog(formatedData);
+
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
-  const fetchBlogsByCategory = () => {
+  const fetchBlogsByCategory = ({ page = 1 }) => {
     axios
-      .post(import.meta.env.VITE_SERVER_DOMAIN + "/search-blogs", { tag: pageState })
-      .then(({ data }) => {
-        setBlog(data.blogs);
+      .post(import.meta.env.VITE_SERVER_DOMAIN + "/search-blogs", { tag: pageState, page })
+      .then( async({ data }) => {
+
+        let formatedData = await filterPaginationData({
+          state: blogs,
+          data: data.blogs,
+          page,
+          countRoute: "/search-blogs-count",
+          data_to_send: { tag: pageState }
+        })
+
+        setBlog(formatedData);
       })
       .catch((err) => {
         console.log(err);
@@ -74,9 +94,9 @@ const HomePage = () => {
     activeTabRef.current.click();
 
     if (pageState == "home") {
-      fetchLatestBlogs();
+      fetchLatestBlogs({ page: 1 });
     } else {
-      fetchBlogsByCategory();
+      fetchBlogsByCategory({ page: 1 });
     }
 
     if(!trendingBlogs) {
@@ -98,8 +118,8 @@ const HomePage = () => {
               {blogs == null ? (
                 <Loader />
               ) : (
-                blogs.length ?
-                  blogs.map((blog, i) => {
+                blogs.results.length ?
+                  blogs.results.map((blog, i) => {
                     return (
                       <AnimationWrapper
                         transition={{ duration: 1, delay: i * 0.1 }}
@@ -114,6 +134,7 @@ const HomePage = () => {
                   })
                 : <NoDataMessage message="No Blogs Published" />
               )}
+              <LoadMoreDataBtn state={blogs} fetchDataFun={(pageState == "home" ? fetchLatestBlogs : fetchBlogsByCategory)} />
             </>
 
             {trendingBlogs == null ? (
