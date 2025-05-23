59d5b6856c332ab81f0458b9acff1003abcb907e
Aditya Raj Lodhi <adityaraj@mail.astroarunpandit.org>
2025-05-24 00:29:49 +0530

    [feat] Add user search functionality with UserCard component integration

--- blogging website - frontend/src/pages/search.page.jsx
+++ blogging website - frontend/src/pages/search.page.jsx
@@ -8,11 +8,13 @@ import NoDataMessage from "../components/nodata.component";
 import LoadMoreDataBtn from "../components/load-more.component";
 import axios from "axios";
 import { filterPaginationData } from "../common/filter-pagination-data";
+import UserCard from "../components/usercard.component";
 
 const SearchPage = () => {
-
   let { query } = useParams();
+
   let [blogs, setBlog] = useState(null);
+  let [users, setUsers] = useState(null);
 
   const searchBlogs = ({ page = 1, create_new_arr = false }) => {
     axios
@@ -27,7 +29,7 @@ const SearchPage = () => {
           page,
           countRoute: "/search-blogs-count",
           data_to_send: { query },
-          create_new_arr
+          create_new_arr,
         });
 
         setBlog(formatedData);
@@ -37,15 +39,44 @@ const SearchPage = () => {
       });
   };
 
+  const fetchUsers = () => {
+    axios
+      .post(import.meta.env.VITE_SERVER_DOMAIN + "/search-users", { query })
+      .then(( { data : { users } } ) => {
+        console.log(users);
+        setUsers(users);
+      })
+  }
+
   useEffect(() => {
 
     resetState();
     searchBlogs({ page: 1, create_new_arr: true });
-    
+    fetchUsers();
+
   }, [query]);
 
-  const resetState = () =>{
+  const resetState = () => {
     setBlog(null);
+    setUsers(null);
+  };
+
+  const UserCardWrapper = () => {
+    return (
+      <>
+        {
+            users == null ? <Loader /> : 
+                users.length ? 
+                    users.map((user, i) => {
+                        return <AnimationWrapper key={i} transition={{ duration: 1, delay: i*0.08 }}>
+                            <UserCard user={user} />
+                        </AnimationWrapper>
+                    })
+            : <NoDataMessage message="No Users Found" />
+            
+        }
+      </>
+    )
   }
 
   return (
@@ -78,8 +109,18 @@ const SearchPage = () => {
             <LoadMoreDataBtn state={blogs} fetchDataFun={searchBlogs} />
           </>
 
+          <UserCardWrapper />
+
         </InPageNavigation>
       </div>
+
+      <div className="min-w-[40%] lg:min-w-[350px] max-w-min border-l border-grey pl-8 pt-3 max-md:hidden">
+        
+        <h1 className="font-medium text-xl mb-8">User related to search <i className="fi fi-rr-user mt-1"></i></h1>
+        <UserCardWrapper />
+
+      </div>
+
     </section>
   );
 };
